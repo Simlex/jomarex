01875cb831392f0cdf62da0e808603255a463cef
Simlex <gettosimlex@gmail.com>
2022-10-05 13:01:10 +0100

    Updated hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Image from 'next/image';
+
+import style from "../styles/hero.module.scss";
+import HeroBg from "../public/heroBg.jpg";
+
+import { MdOutlineAddShoppingCart } from 'react-icons/md';
+
+export interface IHeroProps {
+}
+
+export default function Hero(props: IHeroProps) {
+  return (
+    <div className={style.parent}>
+      <div className={style.bg}>
+        <Image src={HeroBg} />
+      </div>
+      <div className={style.content}>
+        <h2>Let's be your</h2>
+        <span>Baker</span>
+        <p className={style.content__info}>Get the best catering services for all your yummie food and for all events.</p>
+        <button className={style.heroBtn}>
+          <div className={style.heroBtn__icon}>
+            <MdOutlineAddShoppingCart />
+          </div>
+          Book now
+        </button>
+      </div>
+    </div>
+  );
+}
