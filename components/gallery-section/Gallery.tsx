1c1e7ebb9b12bce041c73f57915496e8bb29594d
Simlex <gettosimlex@gmail.com>
2022-10-07 13:07:48 +0100

    Gallery component updated

--- components/gallery-section/Gallery.tsx
+++ components/gallery-section/Gallery.tsx
@@ -1,42 +1,63 @@
 import Image from 'next/image';
-import * as React from 'react';
+import React, { useState } from 'react';
+import { BsFilterCircle } from 'react-icons/bs';
 
 import style from '../../styles/gallery.module.scss';
 import gallery1 from '../../public/gallery/gallery1.jpg';
 
 export interface IGalleryProps {
+    onMobile: any;
 }
 
-export default function Gallery (props: IGalleryProps) {
-  return (
-    <div className={style.parent}>
-        <h2>Our Mouth-Watering Gallery</h2>
-        <div className={style.galleryBox}>
-            <div className={`${style.galleryItem} ${style.img1}`}>
-                <Image src={gallery1} />
+export default function Gallery(props: IGalleryProps) {
+
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+
+    return (
+        <div className={props.onMobile ? style.mParent : style.parent}>
+            <h2>Our Mouth-Watering Gallery</h2>
+            <div className={style.filter}>
+                <div className={style.filterBox} onClick={() => { setDropdownVisible(!dropdownVisible) }}>
+                    <p>Filter</p>
+                    <BsFilterCircle />
+                </div>
+                {
+                    dropdownVisible && (
+                        <div className={style.dropdownBox}>
+                            <p>Snacks</p>
+                            <p>Foods</p>
+                            <p>Pastries</p>
+                        </div>
+                    )
+                }
             </div>
-            <div className={`${style.galleryItem} ${style.img2}`}>
-                <Image src={gallery1} />
-            </div>
-            <div className={`${style.galleryItem} ${style.img3}`}>
-                <Image src={gallery1} />
-            </div>
-            <div className={`${style.galleryItem} ${style.img4}`}>
-                <Image src={gallery1} />
-            </div>
-            <div className={`${style.galleryItem} ${style.img4}`}>
-                <Image src={gallery1} />
-            </div>
-            <div className={`${style.galleryItem} ${style.img4}`}>
-                <Image src={gallery1} />
-            </div>
-            <div className={`${style.galleryItem} ${style.img4}`}>
-                <Image src={gallery1} />
-            </div>
-            <div className={`${style.galleryItem} ${style.img4}`}>
-                <Image src={gallery1} />
+            <div className={props.onMobile ? style.mGalleryBox : style.galleryBox}>
+                <div className={`${style.galleryItem} ${style.img1}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img2}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img3}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img4}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img4}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img4}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img4}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
+                <div className={`${style.galleryItem} ${style.img4}`}>
+                    <Image src={gallery1} alt='Food image' />
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
