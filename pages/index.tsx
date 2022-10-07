a49b80812046baa35db92f4a8d881a543933d25d
Simlex <gettosimlex@gmail.com>
2022-10-07 01:59:28 +0100

    Updated index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import CardContainer from '../components/card-section/CardContainer'
+import Gallery from '../components/gallery-section/Gallery'
 import Hero from '../components/Hero'
+import Whatsapp from '../components/Whatsapp'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
@@ -13,6 +16,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
+      <section>
+        <CardContainer />
+      </section>
+      <section>
+        <Gallery />
+      </section>
+      <Whatsapp />
     </div>
   )
 }
