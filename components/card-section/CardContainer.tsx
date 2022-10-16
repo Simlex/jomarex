53beff127934b50fe9368e538d168e2f8c9da279
Simlex <gettosimlex@gmail.com>
2022-10-16 16:08:44 +0100

    Added scroll animation

--- components/card-section/CardContainer.tsx
+++ components/card-section/CardContainer.tsx
@@ -4,6 +4,8 @@ import Card from './Card';
 import style from '../../styles/card.module.scss';
 import useResponsive from '../useResponsive';
 
+import { motion } from 'framer-motion';
+
 export interface ICardContainerProps {
 }
 
@@ -12,13 +14,18 @@ export default function CardContainer(props: ICardContainerProps) {
     const onMobile = useResponsive();
 
     return (
-        <div className={onMobile ? style.mParent : style.parent}>
+        <motion.div className={onMobile ? style.mParent : style.parent}
+            initial={{y: 30, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{type: 'spring'}}
+            // viewport={{ once: true }}
+            >
             <h2>Our Services</h2>
             <div className={onMobile ? style.mServices : style.services}>
                 <Card onMobile={onMobile} />
                 <Card onMobile={onMobile} />
                 <Card onMobile={onMobile} />
             </div>
-        </div>
+        </motion.div>
     );
 }
